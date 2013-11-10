76ce80d4a370313c1a6d4ef21bb03f5bf83ef625
Ed McDonald <darthsteedious@gmail.com>
2013-11-09 19:02:35 -0500

    Fixing a dup key issue with scout achievements

--- app/models/Scouts.js
+++ app/models/Scouts.js
@@ -2,19 +2,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 module.exports = function (dal, ModelMetadata) {
-    var AchievementSchema = new mongoose.Schema({
-            name: {type: String, required: true, unique: true},
-            description: {type: String, required: true, unique: false},
-            createddate: {type: Date, required: true, unique: false}
-        }),
-        ScoutSchema = new mongoose.Schema(ModelMetadata.applymeta({
+        var ScoutSchema = new mongoose.Schema(ModelMetadata.applymeta({
             firstname: {type: String, required: true, unique: false},
             lastname: {type: String, required: true, unique: false},
             birthdate: {type: Date, required: false, unique: false},
-            rank: {type: String, required: true, unique: false},
-            achievements: [
-                AchievementSchema
-            ],
+//            rank: {type: String, required: true, unique: false},
+//            achievements: [
+//
+//            ],
             akelas: [
                 {type: Schema.Types.ObjectId, ref: 'akelas'}
             ],
