db6d26e704932d7e1f49222a3ceb5a919ca4a73e
johnnymo87 <johnnymo87@gmail.com>
2013-12-10 16:18:22 +0000

    rough draft of unit test for AkelaController#destroy

---
+++ test/AkelaController_test.js
@@ -0,0 +1,11 @@
+var assert          = require('assert'),
+    expect          = require('expect.js'),
+    AkelaController = ('../AkelaController');
+
+describe('AkelaController', function() {
+    describe('destroy', function() {
+        it('should expose a function', function() {
+            expect(destroy).to.be.a('function');
+        })
+    })
+});
