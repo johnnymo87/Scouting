3839be5bdee42f4f032f415cbdd0a0d9d53266fd
johnnymo87 <johnnymo87@gmail.com>
2013-12-10 19:49:14 +0000

    rejoice: mocha can talk to a controller!

--- test/AkelaController_test.js
+++ test/AkelaController_test.js
@@ -1,11 +1,21 @@
 var assert          = require('assert'),
     expect          = require('expect.js'),
-    AkelaController = ('../AkelaController');
+    AkelaController = require('../app/controllers/AkelaController.js');
+
+describe('Jasmine sanity check', function() {
+    it('works', function() {  expect(true).to.be(true); });
+});
 
 describe('AkelaController', function() {
+    var akela;
+
+    beforeEach(function(){
+        akela = new AkelaController();
+    });
+
     describe('destroy', function() {
         it('should expose a function', function() {
-            expect(destroy).to.be.a('function');
+            expect(akela.destroy.get).to.be.a('function');
         })
     })
 });
